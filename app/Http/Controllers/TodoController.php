98b3158e1071fe98acbc5bceb0972a55833d689a
rashedul28 <rashedulislamridoy@gmail.com>
2023-09-27 12:43:58 +0600

    todo apps complete

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+
+        $todo = Todo::all();
+        return view('index')->with('todos', $todo);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function store()
+    {
+        try {
+            $this->validate(request(), ['name' => ['required'], 'description' => ['required']]);
+        } catch (ValidationException $e) {
+        }
+        $data = request()->all();
+        $todo = new Todo();
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->save();
+
+        session()->flash('success', 'Todo created succesfully');
+
+        return redirect('/');
+    }
+    public function details(Todo $todo)
+    {
+
+        return view('details')->with('todos', $todo);
+    }
+
+    public function edit(Todo $todo)
+    {
+        return view('edit')->with('todos', $todo);
+    }
+    public function update(Todo $todo)
+    {
+
+        try {
+            $this->validate(request(), [
+                'name' => ['required'],
+                'description' => ['required'],
+
+            ]);
+        } catch (ValidationException $e) {
+        }
+
+        $data = request()->all();
+
+
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->save();
+
+        session()->flash('success', 'Todo updated successfully');
+
+        return redirect('/');
+    }
+    public function delete(Todo $todo)
+    {
+
+        $todo->delete();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TodoController;
+use App\Models\Todo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class, 'index']);
+
+Route::get('create', [TodoController::class, 'create']);
+Route::post('store-data', [TodoController::class, 'store']);
+
+Route::get('details/{todo}', [TodoController::class, 'details']);
+Route::get('edit/{todo}', [TodoController::class, 'edit']);
+Route::post('update/{todo}', [TodoController::class, 'update']);
+
+Route::get('delete/{todo}', [TodoController::class, 'delete']);
